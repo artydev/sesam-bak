2a4feb99019b0f1816d3899f99e45fae40869952
DeciusAntiochus <awaidaantoine@yahoo.fr>
2019-06-19 10:32:55 +0200

    sql push

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+const registerRoutes = require("./routes/api");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+var port = process.env.PORT || 8080;
+
+registerRoutes(app);
+
+app.listen(port);
+console.log("Started on port " + port);
