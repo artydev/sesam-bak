91c0a836ceb75bf462d4730d429b3d831e47e826
Antoine Paris <antoine.paris@student.ecp.fr>
2019-07-19 19:01:58 +0200

    config

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ module.exports = {
     port: process.env.PORT || 8080,
 
     job:{
-        //launch the job every day at 8h30 (the syntax is 'mm hh * * *') see node-schedule (https://www.npmjs.com/package/node-schedule)
+        //launch the job hour (the syntax is 'mm hh * * *') see node-schedule (https://www.npmjs.com/package/node-schedule)
         job_time : '0 * * * *',
         xml_file_folder:"XmlVisiteSesam/",
         number_of_day: 1, //the number of day to keep the document in couchdb after having been given to sora
     },
 
-    sql_config:{
+    dev_sql_config:{
         user: 'sa',
         password: 'password1&',
         server: 'localhost',
