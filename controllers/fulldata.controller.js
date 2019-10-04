b0549e4fb6e679258f48af9db899c858ce219b75
artyprog <salvatore.didio@gmail.com>
2019-10-04 11:41:34 +0200

    Update SQL Request

--- controllers/fulldata.controller.js
+++ controllers/fulldata.controller.js
@@ -1,40 +1,54 @@
 const {execSql} = require('../sql');
 
+const getQueryTable = (idAgent) => {  
+  return {
+      cpf: {
+          table:'produit-cpf',
+          query:`select * from PRODUIT_CPF`,
+          idField: 'CPF_IDENT'
+      },
+      activite:{
+          table:'activite',
+          query:`select * from ACTIVITE_DG`,
+          idField: 'ACDG_IDENT'
+      },
+      _dossiers: {
+          table:'dossiers',
+          query:`select * from DOSSIER AS D
+          join TACHE_PROGRAMMEE on TACHE_PROGRAMMEE.TAPR_IDENT = D.TAPR_IDENT
+          where RESPONSABLE_AGENT_DD_IDENT = ${idAgent} 
+          and D.DOSSIER_DATE_ENREG >=  DATEADD(YEAR,-2,GETDATE())`,
+          idField: 'DOSSIER_IDENT'
+		},
+	    dossiers: {
+          table:'dossiers',
+          query:`SELECT d.*,T.*  FROM dossier AS d
+			INNER JOIN tache_programmee as T on d.tapr_ident =t.tapr_ident 
+			INNER JOIN AGDD_DOSSIER as AG on AG.DOSSIER_IDENT = d.DOSSIER_IDENT
+			WHERE AGENT_DD_IDENT = ${idAgent}
+			AND DOSSIER_FLAG_CLOTURE = 0`,
+          idField: 'DOSSIER_IDENT'
+      },
+      visites: {
+          table:'visites',
+          query:`select * from VISITE AS V
+          left join ETABLISSEMENT_OBSERVE 
+          on  ETABLISSEMENT_OBSERVE.ETOB_IDENT = V.ETOB_IDENT
+          where AGENT_DD_IDENT = ${idAgent}
+          and V.VISITE_DATE_INTERVENTION >=  DATEADD(YEAR,-2,GETDATE())
+          order by V.VISITE_DATE_INTERVENTION DESC`,
+          idField: 'VISITE_IDENT'
 
-const getQueryTable = (idAgent) => ({
-        cpf: {
-            table:'produit-cpf',
-            query:`select * from PRODUIT_CPF`,
-            idField: 'CPF_IDENT'
-        },
-        activite:{
-            table:'activite',
-            query:`select * from ACTIVITE_DG`,
-            idField: 'ACDG_IDENT'
-        },
-        dossiers: {
-            table:'dossiers',
-            query:`select * from DOSSIER
-            join TACHE_PROGRAMMEE on TACHE_PROGRAMMEE.TAPR_IDENT = DOSSIER.TAPR_IDENT
-            where RESPONSABLE_AGENT_DD_IDENT = ${idAgent}`,
-            idField: 'DOSSIER_IDENT'
-        },
-        visites: {
-            table:'visites',
-            query:`select * from VISITE 
-            left join ETABLISSEMENT_OBSERVE 
-            on  ETABLISSEMENT_OBSERVE.ETOB_IDENT = VISITE.ETOB_IDENT
-            where AGENT_DD_IDENT = ${idAgent}`,
-            idField: 'VISITE_IDENT'
-
-        },
-        controles : {
-            table:'controles',
-            query:`select * from CONTROLE 
-            where AGENT_DD_IDENT = ${idAgent}`,
-            idField: 'CONTROLE_IDENT'
-        }
-    });
+      },
+      controles : {
+          table:'controles',
+          query:`select * from CONTROLE 
+          where AGENT_DD_IDENT = ${idAgent}
+          and VISITE_DATE_INTERVENTION >= DATEADD(YEAR,-2,GETDATE())`,
+          idField: 'CONTROLE_IDENT'
+      }
+    }
+};
 
 module.exports =  getDataByUserId = async (tableKey,idAgent) => {
         // await sql.connect('mssql://sesameTestApp:16amTsTApp!@devirissql\\MSSQL_TSTIRIS/DATAWH');
